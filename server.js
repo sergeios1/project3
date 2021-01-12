b4743bad1763de171c04ead0abb9edbda2b632d8
sergios1 <sergetnazaretyan@gmail.com>
2021-01-11 17:17:29 -0800

    aaaaaaaaaaaaaaa

--- server.js
+++ server.js
@@ -2,23 +2,65 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+let mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log(`Connected to MongoDB😍😍😍 ... `) )
+  .catch(() => console.log(`Could not Connect to MongoDB ... `) );
+            
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+    app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+
+
+let urlSchema = new mongoose.Schema({
+  long: {type: String, required: true},
+  short: Number
+})
+
+Url = mongoose.model("Url", urlSchema);
+let bodyParser = require('body-parser')
+
+
+let shorty = 1;
+
+
+
+
+app.post('/api/shorturl/new', bodyParser.urlencoded({extended: false}) , (req, res) => {
+  let urls = req.body.url 
+  let jojo = new Url({long: urls, short: shorty+=1}) 
+
+  jojo.save((err,data) => {
+    if(err)return console.error(err);
+  })
+
+  res.json({"original_url":urls,"short_url":shorty})
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+app.get('/api/shorturl/:word', (req,res) => {
+  console.log(req.params.word);
+  let thingToFind = req.params.word;
+  Url.find({short: thingToFind}, (err,data) => {
+    if(err)return console.error(err);
+    res.redirect(data[0].long);
+  })
+})
+
